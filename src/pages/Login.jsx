a25f93df50195f1b82b6f6cf886feefa360eb749
Muhammadqodir-st <muhammadqodirov3609@gmail.com>
2025-09-28 06:03:23 +0500

    regiter

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -8,8 +8,8 @@ export default function Login() {
     const location = useLocation();
 
     return (
-        <div className="max-w-[998px] w-[90%] h-200 mx-auto flex items-center justify-center">
-            <div className="flex flex-col gap-8">
+        <div className="max-w-[998px] w-[90%] h-170 mx-auto flex items-center justify-center">
+            <div className="flex flex-col items-center justify-center gap-8 w-full">
                 {/* top navogation */}
                 <div className="flex items-center justify-center gap-5">
                     <Link to={'/auth/login'} className={location.pathname === '/auth/login' ? 'text-4xl font-bold' : 'text-4xl text-[#9CA3AF] font-bold'}>Login</Link>
@@ -17,19 +17,19 @@ export default function Login() {
                 </div>
 
                 {/* form data */}
-                <form className="flex flex-col gap-4">
-                    <p className=" text-[#030712]">If you have an account, sign in with your username or email address.</p>
+                <form className="flex flex-col gap-4 w-[60%] max-[850px]:w-[90%]">
+                    <p className=" text-[#030712] text-center py-2">If you have an account, sign in with your username or email address.</p>
                     {/* username */}
-                    <label>
+                    <label className="flex flex-col gap-1">
                         <p>Username or email address *</p>
-                        <input className="w-full rounded-lg border border-[#D1D5DB] p-2 outline-0" type="text" />
+                        <input className="w-full rounded-lg border border-[#D1D5DB] p-2 outline-0" type="text" autoFocus required />
                     </label>
-                    {/* email */}
-                    <label>
+                    {/* password */}
+                    <label className="flex flex-col gap-1">
                         <p>Password *</p>
-                        <input className="w-full rounded-lg border border-[#D1D5DB] p-2 outline-0" type="password" />
+                        <input className="w-full rounded-lg border border-[#D1D5DB] p-2 outline-0" type="password" required />
                     </label>
-                    <Link className="text-[#1D4ED8] text-end">Lost your password?</Link>
+                    <Link to={'/auth/register'} className="text-[#1D4ED8] text-end">Lost your password?</Link>
                     <button type="submit" className="py-3 rounded-lg bg-[#634C9F] text-white cursor-pointer">Log in</button>
                 </form>
             </div>
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,19 +1,66 @@
 // react router dom
 import { Link, useLocation } from "react-router-dom"
 
+// react
+import { useState } from "react";
+
 
 export default function Register() {
 
     // location
     const location = useLocation();
 
+    // state
+    const [role, setRole] = useState('customer');
+
+
     return (
-        <div className="max-w-[998px] w-[90%] mx-auto flex items-center justify-center">
-            <div>
+        <div className="max-w-[998px] w-[90%] h-190 mx-auto flex items-center justify-center">
+            <div className="flex flex-col items-center justify-center gap-8 w-full">
+                {/* top navogation */}
                 <div className="flex items-center justify-center gap-5">
                     <Link to={'/auth/login'} className={location.pathname === '/auth/login' ? 'text-4xl font-bold' : 'text-4xl text-[#9CA3AF] font-bold'}>Login</Link>
                     <Link to={'/auth/register'} className={location.pathname === '/auth/register' ? 'text-4xl font-bold' : 'text-4xl text-[#9CA3AF] font-bold'}>Register</Link>
                 </div>
+
+                {/* form data */}
+                <form className="flex flex-col gap-4 w-[60%] max-[850px]:w-[90%]">
+                    <p className=" text-[#030712] text-center py-2 max-[850px]:text-sm">There are many advantages to creating an account: the payment process is faster, shipment tracking is possible and much more.</p>
+                    {/* username */}
+                    <label className="flex flex-col gap-1">
+                        <p className="max-[850px]:text-sm">Username *</p>
+                        <input className="w-full rounded-lg border border-[#D1D5DB] p-2 outline-0" type="text" autoFocus required />
+                    </label>
+                    {/* email */}
+                    <label className="flex flex-col gap-1">
+                        <p className="max-[850px]:text-sm">Email address *</p>
+                        <input className="w-full rounded-lg border border-[#D1D5DB] p-2 outline-0" type="email" required />
+                    </label>
+                    {/* password */}
+                    <label className="flex flex-col gap-1">
+                        <p className="max-[850px]:text-sm">Password *</p>
+                        <input className="w-full rounded-lg border border-[#D1D5DB] p-2 outline-0" type="password" required />
+                    </label>
+
+                    {/* role */}
+                    <div className="flex flex-col gap-1">
+                        <label className="flex items-center gap-1">
+                            <input type="radio" onChange={(e) => setRole(e.target.value)} value="customer" checked={role === "customer"} />
+                            <p className="text-sm text-[]">I am a customer</p>
+                        </label>
+                        <label className="flex items-center gap-1">
+                            <input type="radio" onChange={(e) => setRole(e.target.value)} value="vendor" checked={role === "vendor"} />
+                            <p className="text-sm text-[]">I am a vendor</p>
+                        </label>
+                    </div>
+
+                    {/* police */}
+                    <p className="text-sm">Your personal data will be used to support your experience throughout this
+                        website, to manage access to your account, and for other purposes described in our <Link className="text-[#634C9F]">privacy policy</Link>.</p>
+
+                    {/* submit btn */}
+                    <button type="submit" className="py-3 rounded-lg bg-[#634C9F] text-white cursor-pointer">Register</button>
+                </form>
             </div>
         </div>
     )
