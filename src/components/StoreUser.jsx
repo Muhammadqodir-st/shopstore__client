e53492132eaad4b28c868522bb0689f2c8760c18
Muhammadqodir-st <muhammadqodirov3609@gmail.com>
2025-10-23 14:26:03 +0500

    finally push

--- src/components/StoreUser.jsx
+++ src/components/StoreUser.jsx
@@ -39,12 +39,7 @@ export default function StoreUser() {
     // loading 
     if (loading) {
         return (
-            <div className="fixed inset-0 flex items-center justify-center bg-white z-50">
-                <div className="flex flex-col items-center">
-                    <div className="w-12 h-12 border-4 border-blue-500 border-t-transparent rounded-full animate-spin"></div>
-                    <p className="mt-4 text-blue-600 font-semibold text-lg">Loading...</p>
-                </div>
-            </div>
+            <p></p>
         )
     }
 
